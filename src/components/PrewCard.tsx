81b487b547ac2d0e8ec49a6f9bf2aba532bd5b75
MrSeager <seager1993@gmail.com>
2025-01-06 11:55:22 +0200

    1/6/2025

--- src/components/PrewCard.tsx
+++ src/components/PrewCard.tsx
@@ -4,9 +4,9 @@ import './PrewCardStyle.css';
 import PrewCardCol from './PrewCardCol.tsx';
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 //Spring
-import { useSpring, animated } from '@react-spring/web';
+import { useSprings, animated } from '@react-spring/web';
 //Images
 import FirstImg from '../images/icon-sedans.svg';
 import SeconfImg from '../images/icon-suvs.svg';
@@ -32,27 +32,38 @@ const PrewCard: FC = () => {
         par_3: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
     }
 
+    const data = [ 
+        { img: FirstImg, header: lineProps.head_1, par: lineProps.par_1, color: 'orange' }, 
+        { img: SeconfImg, header: lineProps.head_2, par: lineProps.par_2, color: 'dark-cyan' }, 
+        { img: ThirdImg, header: lineProps.head_3, par: lineProps.par_3, color: 'darker-cyan' }, 
+    ]; 
+    
+    const springs = useSprings( 
+        data.length, 
+        data.map((_, i) => ({ 
+            from: { opacity: 0, transform: 'translateX(-150%) scale(0)', borderRadius: '50%' }, 
+            to: { opacity: 1, transform: 'translateX(0%) scale(1)', borderRadius: '0' }, 
+            delay: i * 200,
+            config: {
+                tension: 110,
+            }
+        })) 
+    );
+
     return (
         <Container fluid className='py-5 d-flex flex-column align-items-center justify-content-center min-vh-100'>
-            <Row className='cs-w rounded rounded-3 overflow-hidden'>
-                <PrewCardCol 
-                    img={FirstImg}
-                    header={lineProps.head_1}
-                    par={lineProps.par_1}
-                    color='orange'
-                />
-                <PrewCardCol 
-                    img={SeconfImg}
-                    header={lineProps.head_2}
-                    par={lineProps.par_2}
-                    color='dark-cyan'
-                />
-                <PrewCardCol 
-                    img={ThirdImg}
-                    header={lineProps.head_3}
-                    par={lineProps.par_3}
-                    color='darker-cyan'
-                />
+            <Row className='cs-w rounded rounded-3 overflow-hidden'> 
+                {springs.map((props, index) => ( 
+                    <Col key={index} lg={4} xs={12} className='p-0'> 
+                        <PrewCardCol 
+                            img={data[index].img} 
+                            header={data[index].header} 
+                            par={data[index].par} 
+                            color={data[index].color} 
+                            style={props} 
+                        /> 
+                    </Col> 
+                ))} 
             </Row>
         </Container>
     );
--- src/components/PrewCardCol.tsx
+++ src/components/PrewCardCol.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, CSSProperties } from 'react';
 //Components
 import './PrewCardStyle.css';
 //Bootstrap
@@ -11,16 +11,17 @@ interface PrewCardColProps {
     header: string;
     par: string;
     color: string;
+    style: any;
 }
 
-const PrewCardCol: FC<PrewCardColProps> = ({ img, header, par, color }) => {
+const PrewCardCol: FC<PrewCardColProps> = ({ img, header, par, color, style }) => {
     return (
-        <Col lg={4} xs={12} className={`cs-bg-${color} p-5 d-flex flex-column align-items-start gap-4`}>
+        <animated.div style={style} lg={4} xs={12} className={`cs-bg-${color} h-100 w-100 p-5 d-flex flex-column align-items-start gap-4`}>
             <Image fluid src={img} alt='image logo' />
             <h1 className='cs-ff-bsd cs-fc-white'>{header}</h1>
             <p className='cs-fc-white'>{par}</p>
             <Button className={`cs-btn border-2 rounded-pill px-4 cs-bg-white cs-tc-${color}`}>Learn More</Button>
-        </Col>
+        </animated.div>
     );
 }
 
